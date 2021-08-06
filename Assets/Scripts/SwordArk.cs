5759808e97f34e04109e1412acba0719ba4b9ffc
Asealiea <rosereds2@live.com>
2021-08-06 18:13:35 +1200

    Added jump, attack, jumping attack animations

---
+++ Assets/Scripts/SwordArk.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordArk : MonoBehaviour
+{
+
+    private SpriteRenderer _sprite;
+
+    void Start()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+        if (_sprite == null)
+            Debug.LogError("SwordArk:: SpriteRenderer is null");        
+    }
+
+    public void LayerInFront()
+    {
+        _sprite.sortingOrder = 31;
+    }
+    public void LayerBehind()
+    {
+        _sprite.sortingOrder = 29;
+    }
+
+}
